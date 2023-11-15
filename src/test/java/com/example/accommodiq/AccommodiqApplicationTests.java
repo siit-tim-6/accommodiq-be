cb131f8db8b15d74eebcfda9e2ffb8b763170179
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-15 23:27:34 +0100

    Initial commit

---
+++ src/test/java/com/example/accommodiq/AccommodiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accommodiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
