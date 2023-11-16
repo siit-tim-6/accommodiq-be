d23c54ab65ee2c53e0c2b3b1cf08f8b3c602e8b1
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-16 01:27:46 +0100

    feat: Configure database and fix bugs

--- src/main/java/com/example/accommodiq/domain/User.java
+++ src/main/java/com/example/accommodiq/domain/User.java
@@ -8,6 +8,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    public User() {
+
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
