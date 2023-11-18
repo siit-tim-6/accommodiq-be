e8a12931c0154c38df3fe517abb4e2fd199648cc
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-18 20:39:25 +0100

    feat: add reservation service and getters and setters in reservation class

--- src/main/java/com/example/accommodiq/domain/Reservation.java
+++ src/main/java/com/example/accommodiq/domain/Reservation.java
@@ -16,11 +16,82 @@ public class Reservation {
     private int numberOfGuests;
     private Status status;
 
-    @ManyToOne(cascade = {})
+    @ManyToOne(cascade = {CascadeType.REFRESH})
     private User user;
 
-    @ManyToOne(cascade = {})
+    @ManyToOne(cascade = {CascadeType.REFRESH})
     private Apartment apartment;
+
+    public Reservation() {
+    }
+
+    public Reservation(Long id, Date startDate, Date endDate, int numberOfGuests, Status status, User user, Apartment apartment) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.numberOfGuests = numberOfGuests;
+        this.status = status;
+        this.user = user;
+        this.apartment = apartment;
+    }
+
+    // Create getters and setters...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setApartment(Apartment apartment) {
+        this.apartment = apartment;
+    }
+
+    public Apartment getApartment() {
+        return apartment;
+    }
 
     public enum Status {
         WAITING,
---
+++ src/main/java/com/example/accommodiq/services/ReservationServiceImpl.java
@@ -0,0 +1,82 @@
+package com.example.accommodiq.services;
+
+import com.example.accommodiq.domain.Reservation;
+import com.example.accommodiq.repositories.ReservationRepository;
+import com.example.accommodiq.services.interfaces.IReservationService;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.ResourceBundle;
+
+@Service
+public class ReservationServiceImpl implements IReservationService {
+
+    @Autowired
+    ReservationRepository allReservations;
+
+    ResourceBundle bundle = ResourceBundle.getBundle("ValidationMessages", LocaleContextHolder.getLocale());
+    @Override
+    public Collection<Reservation> getAll() {
+        System.out.println(allReservations.findAll());
+        return allReservations.findAll();
+    }
+
+    @Override
+    public Reservation findReservation(Long reservationId) {
+        Optional<Reservation> found = allReservations.findById(reservationId);
+        if (found.isEmpty()) {
+            String value = bundle.getString("notFound");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, value);
+        }
+        return found.get();
+    }
+
+    @Override
+    public Reservation insert(Reservation reservation) {
+        try {
+            allReservations.save(reservation);
+            allReservations.flush();
+            return reservation;
+        } catch (ConstraintViolationException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "error");
+        }
+    }
+
+    @Override
+    public Reservation update(Reservation reservation) {
+        try {
+            findReservation(reservation.getId()); // this will throw ResponseStatusException if reservation is not found
+            allReservations.save(reservation);
+            allReservations.flush();
+            return reservation;
+        } catch (RuntimeException ex) {
+            Throwable e = ex;
+            Throwable c = null;
+            while((e != null) && !((c=e.getCause()) instanceof ConstraintViolationException))
+                e = (RuntimeException)c;
+            if ((c!= null) && (c instanceof ConstraintViolationException))
+                throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "error");
+            throw ex;
+        }
+    }
+
+    @Override
+    public Reservation delete(Long reservationId) {
+        Reservation found = findReservation(reservationId);
+        allReservations.delete(found);
+        allReservations.flush();
+        return found;
+    }
+
+    @Override
+    public void deleteAll() {
+        allReservations.deleteAll();
+        allReservations.flush();
+    }
+}
