9e50514f2c2106634760743eb76dba2a768b4677
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-20 13:27:23 +0100

    feat: Add Repository annotation

--- src/main/java/com/example/accommodiq/services/interfaces/IAccountService.java
+++ src/main/java/com/example/accommodiq/services/interfaces/IAccountService.java
@@ -1,9 +1,11 @@
 package com.example.accommodiq.services.interfaces;
 
 import com.example.accommodiq.domain.Account;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
+@Repository
 public interface IAccountService {
     public Collection<Account> getAll();
     public Account findAccount(Long accountId);
