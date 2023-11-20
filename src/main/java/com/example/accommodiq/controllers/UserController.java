ca05fbb96d4817cc2450ff0c0ac236e31447329d
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-20 13:56:14 +0100

    feat: change request mapping root in UserController

--- src/main/java/com/example/accommodiq/controllers/UserController.java
+++ src/main/java/com/example/accommodiq/controllers/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/")
 public class UserController {
 
     final
