25d75cc9b3114f6430aed9790dd58375cf1b5533
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-20 11:53:31 +0100

    feat: Create Account model

---
+++ src/main/java/com/example/accommodiq/domain/Account.java
@@ -0,0 +1,81 @@
+package com.example.accommodiq.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+    private String password;
+    private Role role;
+    private AccountStatus status;
+
+    public Account() {
+    }
+
+    public Account(Long id, String email, String password, Role role, AccountStatus status) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public AccountStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(AccountStatus status) {
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public enum Role {
+        GUEST,
+        OWNER,
+        ADMIN
+    }
+
+    public enum AccountStatus {
+        INACTIVE,
+        ACTIVE,
+        BLOCKED
+    }
+}
