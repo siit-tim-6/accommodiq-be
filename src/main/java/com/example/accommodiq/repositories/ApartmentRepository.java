817e57e8c4343b5b515820f948802a1c6374237f
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-20 22:49:56 +0100

    feat: Delete apartment-related classes

--- src/main/java/com/example/accommodiq/repositories/ApartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.accommodiq.repositories;
-
-import com.example.accommodiq.domain.Apartment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
-}
