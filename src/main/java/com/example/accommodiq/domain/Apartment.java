93483d96bdb6443e020e1f0aa1caffb5d8f9b770
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-20 01:33:33 +0100

    feat: Add more Apartment-related fields in Apartment model

--- src/main/java/com/example/accommodiq/domain/Apartment.java
+++ src/main/java/com/example/accommodiq/domain/Apartment.java
@@ -5,8 +5,6 @@ import jakarta.persistence.*;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import java.util.Collection;
-
 @Entity
 public class Apartment {
     @Id
@@ -15,20 +13,100 @@ public class Apartment {
     private String address;
     private int numberOfRooms;
     private boolean accepted;
-
+    private String title;
+    private String description;
+    private String location;
+    private int minGuest;
+    private int maxGuest;
+    private String type;
+    private boolean isPricePerGuest;
+    private int cancellationDeadline;
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private User owner;
 
-    public Apartment() {
-    }
-
-    public Apartment(String address, int numberOfRooms, boolean accepted, User owner) {
+    public Apartment(Long id, String address, int numberOfRooms, boolean accepted, String title, String description, String location, int minGuest, int maxGuest, String type, boolean isPricePerGuest, int cancellationDeadline, User owner) {
+        this.id = id;
         this.address = address;
         this.numberOfRooms = numberOfRooms;
         this.accepted = accepted;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.minGuest = minGuest;
+        this.maxGuest = maxGuest;
+        this.type = type;
+        this.isPricePerGuest = isPricePerGuest;
+        this.cancellationDeadline = cancellationDeadline;
         this.owner = owner;
+    }
+
+    public Apartment() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getMinGuest() {
+        return minGuest;
+    }
+
+    public void setMinGuest(int minGuest) {
+        this.minGuest = minGuest;
+    }
+
+    public int getMaxGuest() {
+        return maxGuest;
+    }
+
+    public void setMaxGuest(int maxGuest) {
+        this.maxGuest = maxGuest;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isPricePerGuest() {
+        return isPricePerGuest;
+    }
+
+    public void setPricePerGuest(boolean pricePerGuest) {
+        isPricePerGuest = pricePerGuest;
+    }
+
+    public int getCancellationDeadline() {
+        return cancellationDeadline;
+    }
+
+    public void setCancellationDeadline(int cancellationDeadline) {
+        this.cancellationDeadline = cancellationDeadline;
     }
 
     public Long getId() {
