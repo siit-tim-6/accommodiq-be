2e6d6a01171f290be2c5e8f43f3ad84184fddbc0
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-21 11:36:20 +0100

    feat: configure ModelMapper as a Spring bean

--- src/main/java/com/example/accommodiq/AccommodiqApplication.java
+++ src/main/java/com/example/accommodiq/AccommodiqApplication.java
@@ -1,11 +1,17 @@
 package com.example.accommodiq;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccommodiqApplication {
 
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(AccommodiqApplication.class, args);
 	}
