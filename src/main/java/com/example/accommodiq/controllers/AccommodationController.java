8ca0adcfd1da4b2de4b3fd5ffe7783c36010db94
Vladimir Popov <vladapopov02@gmail.com>
2023-11-21 13:00:05 +0100

    feat: added request mapping skeletons

--- src/main/java/com/example/accommodiq/controllers/AccommodationController.java
+++ src/main/java/com/example/accommodiq/controllers/AccommodationController.java
@@ -2,10 +2,9 @@ package com.example.accommodiq.controllers;
 
 import com.example.accommodiq.domain.Accommodation;
 import com.example.accommodiq.services.interfaces.IAccommodationService;
+import jdk.jshell.spi.ExecutionControl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -22,5 +21,40 @@ public class AccommodationController {
     @GetMapping()
     public Collection<Accommodation> getAllAccommodations() {
         return accommodationService.findAll();
+    }
+
+    @PutMapping("/{accommodationId}/accept")
+    public void acceptAccommodationChange(@PathVariable Long accommodationId) {
+        return;
+    }
+
+    @PutMapping("/{accommodationId}/deny")
+    public void declineAccommodationChange(@PathVariable Long accommodationId) {
+        return;
+    }
+
+    @GetMapping("/{accommodationId}")
+    public Accommodation getAccommodationDetails(@PathVariable Long accommodationId) {
+        return accommodationService.findById(accommodationId);
+    }
+
+    @PutMapping("/{accommodationId}")
+    public Accommodation updateAccommodation(@PathVariable Long accommodationId) {
+        return accommodationService.findById(accommodationId);
+    }
+
+    @PostMapping("/{accommodationId}/availability")
+    public Accommodation addAccommodationAvailability(@PathVariable Long accommodationId) {
+        return null;
+    }
+
+    @DeleteMapping("/{accommodationId}/availability/{availabilityId}")
+    public Accommodation removeAccommodationAvailability(@PathVariable Long accommodationId, @PathVariable Long availabilityId) {
+        return null;
+    }
+
+    @GetMapping("/{accommodationId}/report")
+    public Accommodation getAccommodationReport(@PathVariable Long accommodationId) {
+        return null;
     }
 }
