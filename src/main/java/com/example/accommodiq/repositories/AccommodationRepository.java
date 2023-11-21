736beb6627e21bea6ffc0a6c8161a6b0638f64b3
Vladimir Popov <vladapopov02@gmail.com>
2023-11-21 22:12:28 +0100

    feat: added AccommodationRepository

--- src/main/java/com/example/accommodiq/repositories/AccommodationRepository.java
+++ src/main/java/com/example/accommodiq/repositories/AccommodationRepository.java
@@ -1,4 +1,10 @@
 package com.example.accommodiq.repositories;
 
-public class AccommodationRepository {
+import com.example.accommodiq.domain.Accommodation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+    
 }
