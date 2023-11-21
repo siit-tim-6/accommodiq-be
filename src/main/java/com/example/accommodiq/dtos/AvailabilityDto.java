63dab3382b9e1c05d19e0f6ec79120cfce441d55
Vladimir Popov <vladapopov02@gmail.com>
2023-11-22 00:04:24 +0100

    feat: add Availability dto

--- src/main/java/com/example/accommodiq/dtos/AvailabilityDto.java
+++ src/main/java/com/example/accommodiq/dtos/AvailabilityDto.java
@@ -1,4 +1,39 @@
 package com.example.accommodiq.dtos;
 
+import java.util.Date;
+
 public class AvailabilityDto {
+    private Date fromDate;
+    private Date toDate;
+    private double price;
+
+    public AvailabilityDto(Date fromDate, Date toDate, double price) {
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.price = price;
+    }
+
+    public Date getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(Date fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public Date getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(Date toDate) {
+        this.toDate = toDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
