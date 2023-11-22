9af7d7f099684d88f0592d2337651a7163efe31d
Vladimir Popov <vladapopov02@gmail.com>
2023-11-22 21:13:24 +0100

    feat: move controller method definitions to service layer, demonstration purposes

--- src/main/java/com/example/accommodiq/controllers/AccommodationController.java
+++ src/main/java/com/example/accommodiq/controllers/AccommodationController.java
@@ -1,7 +1,5 @@
 package com.example.accommodiq.controllers;
 
-import com.example.accommodiq.domain.Accommodation;
-import com.example.accommodiq.domain.Availability;
 import com.example.accommodiq.dtos.*;
 import com.example.accommodiq.services.interfaces.IAccommodationService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +11,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/accommodations")
 public class AccommodationController {
-    private IAccommodationService accommodationService;
+    final private IAccommodationService accommodationService;
 
     @Autowired
     public AccommodationController(IAccommodationService accommodationService) {
@@ -22,20 +20,7 @@ public class AccommodationController {
 
     @GetMapping()
     public Collection<AccommodationListDto> getAllAccommodations() {
-        ArrayList<AccommodationListDto> retVal = new ArrayList<>() {
-            {
-                add(new AccommodationListDto(1L, "City Center Apartment", "https://example.image.com", 4.92,
-                        202, "Novi Sad", 540, 2, 5));
-            }
-
-            {
-                add(new AccommodationListDto(2L, "City Center Apartment", "https://example.image.com", 4.92,
-                        202, "Novi Sad", 540, 2, 5));
-            }
-        };
-        System.out.println(retVal.size());
-
-        return retVal;
+        return accommodationService.findAll();
     }
 
     @PutMapping("/{accommodationId}/accept")
@@ -50,41 +35,7 @@ public class AccommodationController {
 
     @GetMapping("/{accommodationId}")
     public AccommodationDetailsDto getAccommodationDetails(@PathVariable Long accommodationId) {
-        AccommodationDetailsHostDto detailsHostDto = new AccommodationDetailsHostDto(1L, "John Doe", 4.92, 202);
-        ArrayList<Availability> availabilities = new ArrayList<>() {
-            {
-                add(new Availability(1L, new Date(), new Date(), 150.0));
-
-            }
-
-            {
-                add(new Availability(2L, new Date(), new Date(), 120.0));
-            }
-        };
-        ArrayList<AccommodationDetailsReviewDto> detailsReviewDtos = new ArrayList<>() {
-            {
-                add(new AccommodationDetailsReviewDto("John Doe", "Great place!", 4.5, new Date()));
-            }
-
-            {
-                add(new AccommodationDetailsReviewDto("Jane Smith", "Excellent stay!", 5.0, new Date()));
-            }
-        };
-
-        return new AccommodationDetailsDto(
-                1L,
-                "Cozy Cottage",
-                4.8,
-                25,
-                "123 Main St, Cityville",
-                detailsHostDto,
-                "cottage_image.jpg",
-                2,
-                4,
-                availabilities,
-                "A charming cottage with a beautiful garden.",
-                detailsReviewDtos
-        );
+        return accommodationService.findById(accommodationId);
     }
 
     @PutMapping("/{accommodationId}")
@@ -104,16 +55,6 @@ public class AccommodationController {
 
     @GetMapping("/{accommodationId}/report")
     public AccommodationReportDto getAccommodationReport(@PathVariable Long accommodationId) {
-        ArrayList<AccommodationReportRevenueDto> revenueDtos = new ArrayList<>() {
-            {
-                add(new AccommodationReportRevenueDto("January", 8000));
-            }
-
-            {
-                add(new AccommodationReportRevenueDto("February", 8000));
-            }
-        };
-
-        return new AccommodationReportDto(30, revenueDtos);
+        return accommodationService.getAccommodationReport(accommodationId);
     }
 }
--- src/main/java/com/example/accommodiq/services/interfaces/IAccommodationService.java
+++ src/main/java/com/example/accommodiq/services/interfaces/IAccommodationService.java
@@ -1,13 +1,18 @@
 package com.example.accommodiq.services.interfaces;
 
 import com.example.accommodiq.domain.Accommodation;
+import com.example.accommodiq.dtos.AccommodationDetailsDto;
+import com.example.accommodiq.dtos.AccommodationListDto;
+import com.example.accommodiq.dtos.AccommodationReportDto;
 
 import java.util.Collection;
 
 public interface IAccommodationService {
-    public Collection<Accommodation> findAll();
+    public Collection<AccommodationListDto> findAll();
 
-    public Accommodation findById(Long accommodationId);
+    public AccommodationDetailsDto findById(Long accommodationId);
 
     public Accommodation acceptIncomingChanges(Long accommodationId);
+
+    public AccommodationReportDto getAccommodationReport(Long accommodationId);
 }
