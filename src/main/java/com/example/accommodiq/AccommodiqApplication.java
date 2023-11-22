7ef063d1b42801a16ada0edee5652a07abbab843
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-22 17:45:23 +0100

    refactor: remove unused imports

--- src/main/java/com/example/accommodiq/AccommodiqApplication.java
+++ src/main/java/com/example/accommodiq/AccommodiqApplication.java
@@ -1,9 +1,7 @@
 package com.example.accommodiq;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccommodiqApplication {
