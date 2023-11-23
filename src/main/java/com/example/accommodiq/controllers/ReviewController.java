9b36a5ed8590f365cfcd3c0a1bc0a057c6dbae60
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-23 15:52:02 +0100

    feat: add endpoints to review controller

--- src/main/java/com/example/accommodiq/controllers/ReviewController.java
+++ src/main/java/com/example/accommodiq/controllers/ReviewController.java
@@ -1,9 +1,11 @@
 package com.example.accommodiq.controllers;
 
+import com.example.accommodiq.domain.Review;
 import com.example.accommodiq.services.interfaces.IReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/reviews")
@@ -16,4 +18,56 @@ public class ReviewController {
     public ReviewController(IReviewService service) {
         this.service = service;
     }
+
+    @GetMapping
+    public Collection<Review> getReviews() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Review insertReview(@RequestBody Review review) {
+        return service.insert(review);
+    }
+
+    @PutMapping
+    public Review updateReview(@RequestBody Review review) {
+        return service.update(review);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public Review deleteReview(@PathVariable Long reviewId) {
+        return service.delete(reviewId);
+    }
+
+    @DeleteMapping
+    public void deleteAll() {
+        service.deleteAll();
+    }
+
+    @PutMapping("/{reviewId}/accept")
+    public void acceptReview(@PathVariable Long reviewId) {
+    }
+
+    @PutMapping("/{reviewId}/deny")
+    public void denyReview(@PathVariable Long reviewId) {
+    }
+
+    @PutMapping("/{reviewId}/report")
+    public void reportReview(@PathVariable Long reviewId) {
+    }
+
+    @PostMapping("/host/{hostId}")
+    public void addReview(@PathVariable Long hostId, @RequestBody Review review) {
+    }
+
+    @GetMapping("/host/{hostId}")
+    public Collection<Review> getHostReviews(@PathVariable Long hostId) {
+        return service.getHostReviews(hostId);
+    }
+
+    @GetMapping("/accommodation/{accommodationId}")
+    public Collection<Review> getAccommodationReviews(@PathVariable Long accommodationId) {
+        return service.getAccommodationReviews(accommodationId);
+    }
+
 }
