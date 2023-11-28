075693601b5fb443ea62cb1a5f87ac873edfe82c
Vladimir Popov <vladapopov02@gmail.com>
2023-11-29 00:32:48 +0100

    feat: add guest controller

--- src/main/java/com/example/accommodiq/controllers/GuestController.java
+++ src/main/java/com/example/accommodiq/controllers/GuestController.java
@@ -1,4 +1,39 @@
 package com.example.accommodiq.controllers;
 
+import com.example.accommodiq.domain.Accommodation;
+import com.example.accommodiq.domain.Reservation;
+import com.example.accommodiq.dtos.GuestFavoriteDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/guests")
 public class GuestController {
+    @GetMapping("/{guestId}/reservations")
+    public Collection<Reservation> getReservations(@PathVariable Long guestId) {
+        return null;
+    }
+
+    @PostMapping("/{guestId}/reservations")
+    public Reservation addReservation(@PathVariable Long guestId, @RequestBody Reservation reservation) {
+        return reservation;
+    }
+
+    @GetMapping("/{guestId}/favorites")
+    public Collection<Accommodation> getFavorites(@PathVariable Long guestId) {
+        return null;
+    }
+
+    @PostMapping("/{guestId}/favorites")
+    public GuestFavoriteDto addFavorite(@PathVariable Long guestId, @RequestBody GuestFavoriteDto guestFavoriteDto) {
+        return guestFavoriteDto;
+    }
+
+    @DeleteMapping("/{guestId}/favorites/{accommodationId}")
+    @ResponseStatus(HttpStatus.OK)
+    public void removeFavorite(@PathVariable Long guestId, @PathVariable Long accommodationId) {
+
+    }
 }
