a318d44cb193e473d09ae328d73cad1eacc5ea50
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-29 02:16:52 +0100

    feat: create ReviewDto

---
+++ src/main/java/com/example/accommodiq/dtos/ReviewDto.java
@@ -0,0 +1,88 @@
+package com.example.accommodiq.dtos;
+
+import com.example.accommodiq.domain.Review;
+import com.example.accommodiq.enums.ReviewStatus;
+
+import java.util.Date;
+
+public class ReviewDto {
+    private Long id;
+    private int rating;
+    private String comment;
+    private Date date;
+    private ReviewStatus status;
+    private Long guestId;
+
+    public ReviewDto() {
+    }
+
+    public ReviewDto(Long id, int rating, String comment, Date date, ReviewStatus status, Long guestId) {
+        this.id = id;
+        this.rating = rating;
+        this.comment = comment;
+        this.date = date;
+        this.status = status;
+        this.guestId = guestId;
+    }
+
+    public ReviewDto(Review review) {
+        this.id = review.getId();
+        this.rating = review.getRating();
+        this.comment = review.getComment();
+        this.date = review.getDate();
+        this.status = review.getStatus();
+        this.guestId = review.getGuest().getId();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public ReviewStatus getStatus() {
+        return status;
+    }
+
+    public Long getGuestId() {
+        return guestId;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setStatus(ReviewStatus status) {
+        this.status = status;
+    }
+
+    public void setGuestId(Long guestId) {
+        this.guestId = guestId;
+    }
+
+    public Review toReview() {
+        return new Review(id, rating, comment, date, status);
+    }
+}
