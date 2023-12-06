2c8f1d547ecdfb94178dfcd7b2e9d11876adf0bc
Vladimir Popov <vladapopov02@gmail.com>
2023-12-06 23:26:35 +0100

    refactor: remove unused imports

--- src/main/java/com/example/accommodiq/specifications/AccommodationSpecification.java
+++ src/main/java/com/example/accommodiq/specifications/AccommodationSpecification.java
@@ -1,11 +1,7 @@
 package com.example.accommodiq.specifications;
 
 import com.example.accommodiq.domain.Accommodation;
-import com.example.accommodiq.enums.PriceSearch;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
