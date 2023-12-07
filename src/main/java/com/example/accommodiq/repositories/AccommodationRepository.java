d0fdd1453ac4d40146437d918f9401600a09fbc9
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-12-08 00:17:56 +0100

    feat: Make getting host's accommodations from database

--- src/main/java/com/example/accommodiq/repositories/AccommodationRepository.java
+++ src/main/java/com/example/accommodiq/repositories/AccommodationRepository.java
@@ -1,10 +1,14 @@
 package com.example.accommodiq.repositories;
 
 import com.example.accommodiq.domain.Accommodation;
+import com.example.accommodiq.dtos.AccommodationListDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
-    
+
+    Collection<Accommodation> findByHostId(Long hostId);
 }
