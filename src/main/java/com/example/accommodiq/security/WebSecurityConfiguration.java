0c7057e4b432f27fa9e12866f108eefc71cd8fab
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-12-08 00:36:54 +0100

    feat: Update Security configuration

--- src/main/java/com/example/accommodiq/security/WebSecurityConfiguration.java
+++ src/main/java/com/example/accommodiq/security/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.example.accommodiq.security.jwt.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -33,6 +34,7 @@ public class WebSecurityConfiguration {
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/accommodations").permitAll()
                         .requestMatchers("/sessions").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
