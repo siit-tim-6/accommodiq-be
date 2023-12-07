74640fc749cb6696f12cad7beaa7f63d4fcc85de
Teodor Vidakovic <teodorv22@gmail.com>
2023-12-07 16:14:52 +0100

    refactor: make activateAccount method cleaner

--- src/main/java/com/example/accommodiq/controllers/EmailVerificationController.java
+++ src/main/java/com/example/accommodiq/controllers/EmailVerificationController.java
@@ -6,6 +6,7 @@ import com.example.accommodiq.services.interfaces.IAccountService;
 import com.example.accommodiq.services.interfaces.IVerificationTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,20 +33,33 @@ public class EmailVerificationController {
     public ResponseEntity<String> activateAccount(@RequestParam("token") String verificationToken, @RequestParam("accountId") Long accountId) {
         Optional<Account> optionalAccount = Optional.ofNullable(accountService.findAccount(accountId));
 
-        if (optionalAccount.isPresent() && optionalAccount.get().getActivationExpires() > Instant.now().toEpochMilli()) {
+        if (optionalAccount.isPresent()) {
             Account account = optionalAccount.get();
-            if (verificationTokenService.verifyVerificationToken(accountId, account.getEmail(), verificationToken)) {
-                if (account.getStatus() == AccountStatus.INACTIVE) {
-                    account.setStatus(AccountStatus.ACTIVE);
-                    accountService.update(account);
-                    return ResponseEntity.ok("Activation successful");
-                } else {
-                    return ResponseEntity.badRequest().body("Account is already activated");
-                }
 
+            if (isVerificationTokenValid(accountId, account.getEmail(), verificationToken)) {
+                if (account.getStatus() == AccountStatus.INACTIVE) {
+                    if(isActivationLinkActive(account)) {
+                        account.setStatus(AccountStatus.ACTIVE);
+                        accountService.update(account);
+                        return ResponseEntity.ok("Activation successful");
+                    }
+                    else {
+                        accountService.delete(accountId);
+                        return ResponseEntity.badRequest().body("Activation link has expired, please register again");
+                    }
+                }
+                return ResponseEntity.badRequest().body("Account is already activated");
             }
         }
         return ResponseEntity.badRequest().body("Invalid activation link");
+    }
+
+    private boolean isVerificationTokenValid(Long accountId, String accountEmail, String verificationToken) {
+        return verificationTokenService.verifyVerificationToken(accountId, accountEmail, verificationToken);
+    }
+
+    private boolean isActivationLinkActive(Account account) {
+        return account.getActivationExpires() >= Instant.now().toEpochMilli();
     }
 
 }
