158d16ab9af05e0ce235183e221b05facf9b81b6
Vladimir Popov <vladapopov02@gmail.com>
2023-12-10 19:49:57 +0100

    feat: change one day to number of seconds instead of miliseconds

--- src/main/java/com/example/accommodiq/domain/Accommodation.java
+++ src/main/java/com/example/accommodiq/domain/Accommodation.java
@@ -186,7 +186,7 @@ public class Accommodation {
 
     public boolean isAvailable(Long from, Long to) {
         Long finalFrom = from;
-        Long oneDay = (long) (1000 * 60 * 60 * 24);
+        Long oneDay = (long) (60 * 60 * 24);
 
         if (this.available == null) {
             return false;
