a9746d3b0ae169c2d2f7af26c0e603f22a073c6d
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-12-13 14:40:18 +0100

    feat: Add CrossOrigin annotation on SessionController

--- src/main/java/com/example/accommodiq/controllers/SessionController.java
+++ src/main/java/com/example/accommodiq/controllers/SessionController.java
@@ -4,13 +4,11 @@ import com.example.accommodiq.dtos.CredentialsDto;
 import com.example.accommodiq.dtos.LoginResponseDto;
 import com.example.accommodiq.services.interfaces.ISessionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/sessions")
+@CrossOrigin
 public class SessionController {
     private final ISessionService sessionService;
     @Autowired
