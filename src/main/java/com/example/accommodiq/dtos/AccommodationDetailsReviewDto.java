6440ba86c805420c8628a15ac4fa7234b553626b
Vladimir Popov <vladapopov02@gmail.com>
2023-12-13 22:18:42 +0100

    feat: add review to accommodationdetailsreviewdto constructor conversion

--- src/main/java/com/example/accommodiq/dtos/AccommodationDetailsReviewDto.java
+++ src/main/java/com/example/accommodiq/dtos/AccommodationDetailsReviewDto.java
@@ -1,18 +1,27 @@
 package com.example.accommodiq.dtos;
 
+import com.example.accommodiq.domain.Review;
+
 import java.util.Date;
 
 public class AccommodationDetailsReviewDto {
     private String author;
     private String comment;
     private double rating;
-    private Date date;
+    private Long date;
 
-    public AccommodationDetailsReviewDto(String author, String comment, double rating, Date date) {
+    public AccommodationDetailsReviewDto(String author, String comment, double rating, Long date) {
         this.author = author;
         this.comment = comment;
         this.rating = rating;
         this.date = date;
+    }
+
+    public AccommodationDetailsReviewDto(Review review) {
+        this.author = review.getGuest().getFirstName() + review.getGuest().getLastName();
+        this.comment = review.getComment();
+        this.rating = review.getRating();
+        this.date = review.getDate();
     }
 
     public String getAuthor() {
@@ -39,11 +48,11 @@ public class AccommodationDetailsReviewDto {
         this.rating = rating;
     }
 
-    public Date getDate() {
+    public Long getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(Long date) {
         this.date = date;
     }
 }
