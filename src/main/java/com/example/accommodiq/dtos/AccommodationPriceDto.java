bc988fd00920520d6cce773c7d593beee016211f
Vladimir Popov <vladapopov02@gmail.com>
2023-12-17 01:17:05 +0100

    feat: add accommodationpricedto

--- src/main/java/com/example/accommodiq/dtos/AccommodationPriceDto.java
+++ src/main/java/com/example/accommodiq/dtos/AccommodationPriceDto.java
@@ -1,4 +1,21 @@
 package com.example.accommodiq.dtos;
 
 public class AccommodationPriceDto {
+    private double totalPrice;
+
+    public AccommodationPriceDto() {
+        super();
+    }
+
+    public AccommodationPriceDto(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
