9b6ad27e620f867672263e939e057a0fe5ae42f0
Teodor Vidakovic <teodorv22@gmail.com>
2023-12-20 19:12:44 +0100

    feat: catch DisabledException and BadCredentialsException separately

--- src/main/java/com/example/accommodiq/services/SessionServiceImpl.java
+++ src/main/java/com/example/accommodiq/services/SessionServiceImpl.java
@@ -8,6 +8,8 @@ import com.example.accommodiq.services.interfaces.ISessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -33,22 +35,28 @@ public class SessionServiceImpl implements ISessionService {
 
     @Override
     public LoginResponseDto login(CredentialsDto credentialsDto) {
-        UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(credentialsDto.getEmail(),
-                credentialsDto.getPassword());
-        Authentication auth = authenticationManager.authenticate(authReq);
+        try {
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(credentialsDto.getEmail(), credentialsDto.getPassword())
+            );
+            SecurityContext sc = SecurityContextHolder.getContext();
+            sc.setAuthentication(auth);
 
-        SecurityContext sc = SecurityContextHolder.getContext();
-        sc.setAuthentication(auth);
 
+            Optional<? extends GrantedAuthority> s = sc.getAuthentication().getAuthorities().stream().findFirst();
+            if (s.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
 
-        Optional<? extends GrantedAuthority> s = sc.getAuthentication().getAuthorities().stream().findFirst();
-        if (s.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+            AccountRole role = (AccountRole) s.get();
+
+            String token = jwtTokenUtil.generateToken(credentialsDto.getEmail(), role);
+            return new LoginResponseDto(role, token);
+
+        } catch (DisabledException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Account is not active");
+        } catch (BadCredentialsException e) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
         }
-
-        AccountRole role = (AccountRole) s.get();
-
-        String token = jwtTokenUtil.generateToken(credentialsDto.getEmail(), role);
-        return new LoginResponseDto(role, token);
     }
 }
