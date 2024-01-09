c9ecd2a1f20c2c2dd1c331a2b6da10b09d70932b
Teodor Vidakovic <teodorv22@gmail.com>
2024-01-09 23:14:56 +0100

    refact: remove unused fields

--- src/main/java/com/example/accommodiq/dtos/ReportDto.java
+++ src/main/java/com/example/accommodiq/dtos/ReportDto.java
@@ -1,15 +1,7 @@
 package com.example.accommodiq.dtos;
 
 public class ReportDto {
-    private Long id;
     private String reason;
-    private Long timestamp;
-
-    public ReportDto(Long id, String reason, Long timestamp) {
-        this.id = id;
-        this.reason = reason;
-        this.timestamp = timestamp;
-    }
 
     public ReportDto(String reason) {
         this.reason = reason;
@@ -22,13 +14,4 @@ public class ReportDto {
     public String getReason() {
         return reason;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Long getTimestamp() {
-        return timestamp;
-    }
-
 }
