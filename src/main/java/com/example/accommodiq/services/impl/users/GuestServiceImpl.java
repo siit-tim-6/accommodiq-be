8988093adbb506ea987043b80fcedccf4cdf4a34
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-01-11 01:09:25 +0100

    refactor: rename 'cancelable' into 'cancellable' in corresponding methods

--- src/main/java/com/example/accommodiq/services/impl/users/GuestServiceImpl.java
+++ src/main/java/com/example/accommodiq/services/impl/users/GuestServiceImpl.java
@@ -74,13 +74,13 @@ public class GuestServiceImpl implements IGuestService {
     }
 
     @Override
-    public Collection<Long> getCancelableReservationIds() {
+    public Collection<Long> getCancellableReservationIds() {
         Long guestId = getGuestId();
         List<Reservation> reservations = reservationRepository.findByStatusAndGuestIdAndEndDateGreaterThanOrderByStartDateDesc(ReservationStatus.ACCEPTED, guestId, Instant.now().getEpochSecond());
-        return reservations.stream().filter(this::isCancelable).map(Reservation::getId).toList();
+        return reservations.stream().filter(this::isCancellable).map(Reservation::getId).toList();
     }
 
-    private boolean isCancelable(Reservation reservation) {
+    private boolean isCancellable(Reservation reservation) {
         return reservation.getStartDate() - reservation.getAccommodation().getCancellationDeadline() * 24 * 60 * 60L < Instant.now().toEpochMilli(); // change to milliseconds
     }
 
