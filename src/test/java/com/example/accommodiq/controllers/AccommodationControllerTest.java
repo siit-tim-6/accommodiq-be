af8c1eba994eb95c7ced139aa183a38c8fe6d905
Teodor Vidakovic <teodorv22@gmail.com>
2024-01-16 00:39:31 +0100

    fix: correct name of test properties file

---
+++ src/test/java/com/example/accommodiq/controllers/AccommodationControllerTest.java
@@ -0,0 +1,10 @@
+package com.example.accommodiq.controllers;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccommodationControllerTest {
+    
+}
