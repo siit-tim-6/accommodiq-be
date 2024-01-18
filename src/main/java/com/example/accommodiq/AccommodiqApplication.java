0ca189f8ac110d8d6dc1911722ff935c73753fd8
Vladimir Popov <vladapopov02@gmail.com>
2024-01-18 21:05:31 +0100

    feat: add necessary validation stuff

--- src/main/java/com/example/accommodiq/AccommodiqApplication.java
+++ src/main/java/com/example/accommodiq/AccommodiqApplication.java
@@ -1,13 +1,22 @@
 package com.example.accommodiq;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AccommodiqApplication {
+    @Bean
+    public Validator validator() {
+        ValidatorFactory validatorFactory = Validation.byDefaultProvider().configure().buildValidatorFactory();
+        return validatorFactory.getValidator();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccommodiqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AccommodiqApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/example/accommodiq/validation/ValidationErrorsHandler.java
@@ -0,0 +1,32 @@
+package com.example.accommodiq.validation;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+globalna komponenta za obradu gresaka
+ */
+@RestControllerAdvice
+public class ValidationErrorsHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+}
